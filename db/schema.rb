4b8b72494e1c221a9d23110fda787c0af24b66b5
Naghmeh Shirazi <naghmehd@gmail.com>
2016-05-06 21:40:53 -0500

    added gem refile-postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506163801) do
+ActiveRecord::Schema.define(version: 20160507023927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20160506163801) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
+
+  create_table "refile_attachments", force: :cascade do |t|
+    t.string "namespace", null: false
+  end
+
+  add_index "refile_attachments", ["namespace"], name: "index_refile_attachments_on_namespace", using: :btree
 
   create_table "toys", force: :cascade do |t|
     t.string   "name",             null: false
