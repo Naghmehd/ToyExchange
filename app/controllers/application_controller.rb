f7a4040bc643a8f60041ca157eae4c432aa124dc
Naghmeh Shirazi <naghmehd@gmail.com>
2016-05-07 13:29:20 -0500

    put the scema in application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   respond_to :html, :json
-  config.active_record.schema_format = :sql
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module ToyExchange
   class Application < Rails::Application
+    config.active_record.schema_format = :sql
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
