9474aeca87cf414f76af8080824353241dcf9fc8
Naghmeh Shirazi <naghmehd@gmail.com>
2016-05-07 17:22:38 -0500

    added model for message and sender and recipient to the conversation model

---
+++ app/models/message.rb
@@ -0,0 +1,8 @@
+class Message < ActiveRecord::Base
+ belongs_to :conversation
+ belongs_to :user
+ validates_presence_of :body, :conversation_id, :user_id
+ def message_time
+  created_at.strftime(“%m/%d/%y at %l:%M %p”)
+ end
+end
