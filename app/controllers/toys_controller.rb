ccbbeb74376adc8654f924a4b4c82c38b58b4452
Naghmeh Shirazi <naghmehd@gmail.com>
2016-05-09 16:36:24 -0500

    can add messages

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,78 +1,96 @@
 class ToysController < ApplicationController
 
-    def index
-      @toys = Toy.all
+  def index
+    @toys = Toy.all
+    respond_to do |format|
+      format.html { }
+      format.json { render json: @toys }
+    end
+  end
+
+  # def toys
+  #   all_toys = Toy.all.sort_by { |t| [t.name]}.reverse!
+  #   current_page = params[:page].toy_id
+  #   if current_page <= 1
+  #     start_number = 0
+  #     end_number = 10
+  #   else
+  #     start_number = current_page + 10
+  #     end_number = current_page * 10
+  #   end
+  #
+  #   @toys = all_toys[start_number..end_number]
+  #   respond_to do |format|
+  #     format.html { }
+  #     format.json { render json: @toys }
+  #   end
+  # end
+
+  def show
+    @toy = Toy.find(params[:id])
+    @conversation = Conversation.new
+  end
+
+  def new
+    @toys = Toy.new
+  end
+
+  def create
+    @toy = current_user.toys.build(toy_params)
+    @user = current_user
+
       respond_to do |format|
-        format.html { }
-        format.json { render json: @toys }
-      end
-    end
-
-    def show
-      @toy = Toy.find(params[:id])
-      @conversation = Conversation.new
-    end
-
-    def new
-      @toys = Toy.new
-    end
-
-    def create
-      @toy = current_user.toys.build(toy_params)
-      @user = current_user
-
-        respond_to do |format|
-           if @toy.save
-             format.html { redirect_to user_path(@user), notice: 'Toy was successfully created.' }
-             format.js   { }
-             format.json { render :show, status: :created, location: @toy }
-           else
-             format.html { render :new_toy_path }
-             format.json { render json: @toy.errors, status: :unprocessable_entity}
-           end
+         if @toy.save
+           format.html { redirect_to user_path(@user), notice: 'Toy was successfully created.' }
+           format.js   { }
+           format.json { render :show, status: :created, location: @toy }
+         else
+           format.html { render :new_toy_path }
+           format.json { render json: @toy.errors, status: :unprocessable_entity}
          end
+       end
+  end
+
+
+  def edit
+    @toy = get_toy
+  end
+
+  def update
+    @toy = get_toy
+
+    if @toy.update(toy_params)
+      redirect_to @toy_path
+    else
+      render :edit
     end
+  end
 
+  def publish
+    @toy = get_toy
+    @toy.published = true
+    @toy.save!
 
-    def edit
-      @toy = get_toy
-    end
+    flash[:notice] = "List the Toy!"
+    redirect_to :back
+  end
 
-    def update
-      @toy = get_toy
+  def destroy
+    @toy = get_toy
+    if @toy.destroy
 
-      if @toy.update(toy_params)
-        redirect_to @toy_path
-      else
-        render :edit
-      end
-    end
-
-    def publish
-      @toy = get_toy
-      @toy.published = true
-      @toy.save!
-
-      flash[:notice] = "List the Toy!"
+      redirect_to :back
+    else
       redirect_to :back
     end
+  end
 
-    def destroy
-      @toy = get_toy
-      if @toy.destroy
+private
+  def get_toy
+    @toy = Toy.find(params.fetch(:id))
+  end
 
-        redirect_to :back
-      else
-        redirect_to :back
-      end
-    end
-
-  private
-    def get_toy
-      @toy = Toy.find(params.fetch(:id))
-    end
-
-    def toy_params
-      params.require(:toy).permit(:name, :brand, :category, :description, :condition, :age_group, :picture_url, :profile_image)
-    end
+  def toy_params
+    params.require(:toy).permit(:name, :brand, :category, :description, :condition, :age_group, :picture_url, :profile_image)
+  end
 end
