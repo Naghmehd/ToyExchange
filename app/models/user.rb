08dabcc7f78176f07b90183697347c0b14d81630
Naghmeh Shirazi <naghmehd@gmail.com>
2016-05-16 16:28:36 -0500

    commented the location and image_url

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,8 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth_hash)
     user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
     user.username = auth_hash['info']['name']
-    user.location = auth_hash['info']['location']
-    user.image_url = auth_hash['info']['image']
+    # user.location = auth_hash['info']['location']
+    # user.image_url = auth_hash['info']['image']
     user.url = auth_hash['info']['urls'][user.provider.capitalize]
     user.save!
     user
