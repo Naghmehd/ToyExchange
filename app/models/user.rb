89425938b507224a53633ca4560f819786e42eaa
Naghmeh Shirazi <naghmehd@gmail.com>
2016-05-16 16:48:36 -0500

    Fixing auth issue.

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,8 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth_hash)
     where(provider: auth_hash["provider"], uid: auth_hash["uid"]).first_or_initialize do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
+      user.provider = auth_hash["provider"]
+      user.uid = auth_hash["uid"]
       user.email = auth_hash['info']['email']
       user.username = auth_hash['info']['name']
       user.password = Devise.friendly_token[0,20]
