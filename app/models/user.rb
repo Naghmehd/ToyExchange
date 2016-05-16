06232edebf9fa990771867e2438e74701a6c154c
Naghmeh Shirazi <naghmehd@gmail.com>
2016-05-16 16:25:49 -0500

    added username to the user model for fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth_hash)
     user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
-    user.name = auth_hash['info']['name']
+    user.username = auth_hash['info']['name']
     user.location = auth_hash['info']['location']
     user.image_url = auth_hash['info']['image']
     user.url = auth_hash['info']['urls'][user.provider.capitalize]
