39250041f1d723dab272283c8180f0cb747a9c2b
Naghmeh Shirazi <naghmehd@gmail.com>
2016-08-05 17:16:16 -0500

    added view file to users index.html.erb
    added json to the users controller index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    respond_to do |format|
+      format.json { render json: @users.to_json }
+      format.html
+    end
   end
 
 
